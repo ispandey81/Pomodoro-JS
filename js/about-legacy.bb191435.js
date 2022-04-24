0184271dd6491458d0a01cbe6d349d60b434b28d
indra <ispandey81@gmail.com>
2022-04-24 15:17:52 +0800

    deploy

---
+++ js/about-legacy.bb191435.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpomodoro_js"]=self["webpackChunkpomodoro_js"]||[]).push([[443],{8005:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var o=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v(" This is an implementation of pomodoro timer in vue using a web worker ")])])}],s=t(1001),u={},i=(0,s.Z)(u,o,r,!1,null,null,null),l=i.exports}}]);
+//# sourceMappingURL=about-legacy.bb191435.js.map
